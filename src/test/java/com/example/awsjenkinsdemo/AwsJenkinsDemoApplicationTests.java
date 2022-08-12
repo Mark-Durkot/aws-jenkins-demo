76503a39f007d7334f3ae97f57e419e1fd06f0f9
Mark-Durkot <106492009+Studio-Jam@users.noreply.github.com>
2022-08-12 17:57:38 +0200

    Init Commit

---
+++ src/test/java/com/example/awsjenkinsdemo/AwsJenkinsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awsjenkinsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsJenkinsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
