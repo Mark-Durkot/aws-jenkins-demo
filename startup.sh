d6e6fbdbf1bf7c5f2f45b31abe6b195ce9bd3d46
Mark Durkot <mark.durkot@gmail.com>
2022-08-16 20:41:56 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+echo 'On port 8081 before kill'
+lsof -i:8081
+
 kill $(lsof -t -i:8081)
+
+echo 'On port 8081 after kill'
+lsof -i:8081
+
 nohup java -jar aws-jenkins-demo-0.0.1-SNAPSHOT.jar > ~/log.txt 2>&1 &
 echo $! > pid.file
+
+echo 'On port 8081 after start'
+lsof -i:8081
