3fe37f07bdb6dc79c82600a63e12273cd73e7bfe
Mark Durkot <mark.durkot@gmail.com>
2022-08-16 21:21:42 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo 'Proccesses on port 8081 before cleaning:'
+sudo lsof -i:8080
+
+kill -15 < pid.file
+
+echo 'Proccesses on port 8081 after cleaning:'
+sudo lsof -i:8080
+
 kill -15 $(sudo lsof -t -i:8081)
 
 nohup java -jar aws-jenkins-demo-0.0.1-SNAPSHOT.jar > ~/log.txt 2>&1 &
